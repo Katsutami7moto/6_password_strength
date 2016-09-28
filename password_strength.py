d1a19ce5a0d6ee290879a14ba7610f38679464b6
Katsutami7moto <nclsf87@gmail.com>
2016-09-29 01:22:11 +0300

    Scores 1 and 3 are passing

--- password_strength.py
+++ password_strength.py
@@ -1,3 +1,6 @@
+import re
+
+
 def is_common(password):
     pass
 
@@ -24,28 +27,31 @@ def is_weak(password):
 
 
 def only_digits(password):
-    for symbol in password:
-        if not symbol.isdigit():
-            return False
-    return True
+    return re.fullmatch(r"[0-9]+", password)
+
+
+def is_ascii(password):
+    return re.fullmatch(r"[a-zA-Z0-9_`~!@#$%\^&*()\-+=/{\}\[\]\\|;':\",.<>?]+", password)
 
 
 def get_password_strength(password):
     def pass_len(number):
         return len(password) > number
 
+    assert is_ascii(password)
+
     if len(password) < 6:
         return tuple([1, "Your password should be 6 or more symbols long."])
     if is_weak(password):
         return tuple([2, "You are using one of common passwords or an english word inside your password."])
     if only_digits(password) and pass_len(5):
-        return tuple([3, "Your password consists only of 0-9 digits."
+        return tuple([3, "Your password consists only of 0-9 digits. \n"
                          "Add lowercase and uppercase latin letters and special symbols (~, @, # and so on)."])
     pass
 
 
 def display(message):
-    print("Mark: {0}.\n{1}".format(*message))
+    print("Score: {0} out of 10.\n{1}".format(*message))
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,4 +1,17 @@
-from password_strength import get_password_strength
+from password_strength import get_password_strength, is_ascii
+
+all_ascii = "\
+`1234567890-=\
+qwertyuiop[]\\\
+asdfghjkl;'\
+zxcvbnm,./\
+~!@#$%^&*()_+\
+QWERTYUIOP{}|\
+ASDFGHJKL:\"\
+ZXCVBNM<>?\
+"
+
+assert is_ascii(all_ascii)
 
 # Testing with short passwords
 assert get_password_strength('12345')[0] == 1
@@ -8,4 +21,9 @@ assert get_password_strength('12')[0] == 1
 assert get_password_strength('1')[0] == 1
 
 # Testing with weak passwords
+pass
 
+# Testing with digits
+assert get_password_strength('272726654989')[0] == 3
+assert get_password_strength('789456')[0] == 3
+assert get_password_strength('000000000')[0] == 3
