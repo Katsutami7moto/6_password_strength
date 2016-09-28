00ec4b138eec1962e2ed5fc428a65b71478bac35
Katsutami7moto <nclsf87@gmail.com>
2016-09-28 20:16:08 +0300

    First steps

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,18 @@
 
+def is_weak(password):
+    pass
+
 
 def get_password_strength(password):
+    if len(password) < 6:
+        print(" Mark: 1. \n Your password should be 6 or more symbols long.")
+        return
+    if is_weak(password):
+        print(" Mark: 2. \n You are using one of common passwords.")
+        return
     pass
 
 
 if __name__ == '__main__':
-    pass
+    psswrd = input("Enter a password: ")
+    get_password_strength(psswrd)
