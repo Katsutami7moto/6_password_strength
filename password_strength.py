675c344830069a4da0c572bcb5056d9bd84ad7ae
Katsutami7moto <nclsf87@gmail.com>
2016-10-03 22:10:51 +0300

    Rewritten get_password_strength()

--- password_strength.py
+++ password_strength.py
@@ -107,7 +107,7 @@ def has_upper(password):
     return re.search(r"[A-Z]", password)
 
 
-def get_password_strength(password) -> tuple:
+def get_password_strength(password) -> list:
     assert is_ascii(password)
     conditions = (
         only_digits(password),
@@ -115,25 +115,29 @@ def get_password_strength(password) -> tuple:
         has_lower(password),
         has_digits(password),
         has_upper(password),
-        has_special(password)
+        has_special(password),
+        has_digits(password) and not has_upper(password) and not has_special(password),
+        not has_digits(password) and has_upper(password) and not has_special(password),
+        has_digits(password) and has_upper(password) and not has_special(password),
+        has_digits(password) and has_upper(password) and has_special(password)
     )
     score = 0
-    text = None
+    text = []
 
     def count_entropy_bits():
         bits_for_symbol = 0
         if conditions[0]:
             bits_for_symbol = 3.3219
-        if conditions[1]:
+        elif conditions[1]:
             bits_for_symbol = 4.7004
-        if conditions[2]:
-            if conditions[3] and not conditions[4] and not conditions[5]:
+        elif conditions[2]:
+            if conditions[6]:
                 bits_for_symbol = 5.1699
-            if not conditions[3] and conditions[4] and not conditions[5]:
+            elif conditions[7]:
                 bits_for_symbol = 5.7004
-            if conditions[3] and conditions[4] and not conditions[5]:
+            elif conditions[8]:
                 bits_for_symbol = 5.9542
-            if conditions[3] and conditions[4] and conditions[5]:
+            elif conditions[9]:
                 bits_for_symbol = 6.5699
         return bits_for_symbol * len(password)
 
@@ -146,75 +150,51 @@ def get_password_strength(password) -> tuple:
 
     if is_weak(password) or is_weak(simple_leet_decoding(password)):
         score = 0
-        text = ("Your password is very weak.\n",
+        text = ["Your password is very weak.\n",
                 "Don't use:\n",
                 "- Simple passwords, like '1234567', 'password00', 'adm1n', etc.\n",
                 "- English words inside your passwords, even with replacing some characters with digits.\n",
-                "- Letter sequences like 'abcdef', 'qwertyuiop', etc.",
-                "- Calendar dates like '131298', '12.28.78', '15/01/2014', etc.",
-                "- Digit sequences like Pi or Fibonacci numbers.")
+                "- Letter sequences like 'abcdef', 'qwertyuiop', etc.\n",
+                "- Calendar dates like '131298', '12.28.78', '15/01/2014', etc.\n",
+                "- Digit sequences like Pi or Fibonacci numbers.\n"]
     elif conditions[0]:
         score = 1
-        text = ("Your password consists only of 0-9 digits.\n",
-                "Add lowercase and uppercase latin letters and special symbols (~, @, #, etc.).")
+        text = ["Your password consists only of 0-9 digits.\n",
+                "Add lowercase and uppercase latin letters and special symbols (~, @, #, etc.).\n"]
     elif conditions[1]:
         score = 2
-        text = ("Your password consists only of lowercase latin letters.\n",
-                "Add 0-9 digits, uppercase latin letters and special symbols (~, @, #, etc.).")
-    elif conditions[3]:
+        text = ["Your password consists only of lowercase latin letters.\n",
+                "Add 0-9 digits, uppercase latin letters and special symbols (~, @, #, etc.).\n"]
+    elif conditions[2]:
         score = 2
+        if conditions[3]:
+            score += 1
         if conditions[4]:
             score += 1
         if conditions[5]:
             score += 1
-        if conditions[6]:
-            score += 1
         score += count_entropy_bonus(count_entropy_bits())
+        if conditions[6]:
+            text = ["Your password consists only of 0-9 digits and lowercase latin letters.\n",
+                    "Add uppercase latin letters and special symbols (~, @, #, etc.).\n"]
+        elif conditions[7]:
+            text = ["Your password consists only of lowercase and uppercase latin letters.\n",
+                    "Add 0-9 digits and special symbols (~, @, #, etc.).\n"]
+        elif conditions[8]:
+            text = ["Your password consists only of 0-9 digits, lowercase and uppercase latin letters.\n",
+                    "Add special symbols (~, @, #, etc.).\n"]
+        elif conditions[9]:
+            text = ["This is good password! :)\n"]
         if score > 10:
             score = 10
-    return score, "".split(text)
-
-# if len(password) < 6:
-#     return 1, "Your password's length should be more than 6."
-# if is_weak(password) or is_weak(simple_leet_decoding(password)):
-#     return (2, "Your password is very weak.\n"
-#                "Don't use:\n"
-#                "- Simple passwords, like '1234567', 'password00', 'adm1n', etc.\n"
-#                "- English words inside your passwords, even with replacing some characters with digits.\n"
-#                "- Calendar dates or digit sequences like Pi or Fibonacci numbers.")
-# if pass_bigger(5):
-#     if only_digits(password):
-#         return (3, "Your password consists only of 0-9 digits.\n"
-#                    "Add lowercase and uppercase latin letters and special symbols (~, @, #, etc.).")
-#     if only_lower(password):
-#         return (4, "Your password consists only of lowercase latin letters.\n"
-#                    "Add 0-9 digits, uppercase latin letters and special symbols (~, @, #, etc.).")
-# if pass_bigger(9) and not has_special(password):
-#     if has_digits(password) and has_lower(password) and not has_upper(password):
-#         return (5, "Your password consists only of 0-9 digits and lowercase latin letters.\n"
-#                    "Add uppercase latin letters and special symbols (~, @, #, etc.).")
-#     if not has_digits(password) and has_lower(password) and has_upper(password):
-#         return (6, "Your password consists only of lowercase and uppercase latin letters.\n"
-#                    "Add 0-9 digits and special symbols (~, @, #, etc.).")
-#     if has_digits(password) and has_lower(password) and has_upper(password):
-#         return (7, "Your password consists only of 0-9 digits, lowercase and uppercase latin letters.\n"
-#                    "Add special symbols (~, @, #, etc.).")
-# if (has_digits(password) or has_lower(password) or has_upper(password))\
-#         and (not pass_bigger(9) and not has_special(password)):
-#     return (4, "Your password's length should be more than 9 to have bigger strength.\n"
-#                "Also you should add special symbols (~, @, #, etc.).")
-# if has_special(password) and has_digits(password) and has_lower(password) and has_upper(password):
-#     if pass_bigger(12) and not pass_bigger(15) and not pass_bigger(18):
-#         return 8, "This is good password! Add more symbols to make it stronger."
-#     elif pass_bigger(15) and not pass_bigger(18):
-#         return 9, "This is great password! Add more symbols to make it stronger."
-#     elif pass_bigger(18):
-#         return 10, "This is awesome password! Add more symbols to make it stronger."
-#     else:
-#         return 7, "Your password's length should be more than 12 to have bigger strength."
+            text = ["Awesome password!\n",
+                    "You don't need to do anything with it :)\n"]
+        else:
+            text.append("You may add more symbols to your password to make it stronger.\n")
+    return score, "".join(text)
 
 
-def display(message: tuple):
+def display(message: list):
     print("Password's strength: {0} out of 10.\n{1}\n".format(*message))
 
 
