101fbc1bc95fe540fba90c1e57e24eb6a050dbba
Katsutami7moto <nclsf87@gmail.com>
2016-10-07 16:53:45 +0300

    Renamed variables

--- install_blacklists.py
+++ install_blacklists.py
@@ -1,9 +1,9 @@
 import urllib.request
 
-blck = 'https://raw.githubusercontent.com/danielmiessler/'
-lst = 'SecLists/master/Passwords/10_million_password_list_top_1000000.txt'
-blcklst = blck + lst
-urllib.request.urlretrieve(url=blcklst, filename='blacklist_1kk.txt')
+blacklist_head = 'https://raw.githubusercontent.com/danielmiessler/'
+blacklist_tail = 'SecLists/master/Passwords/10_million_password_list_top_1000000.txt'
+blacklist = blacklist_head + blacklist_tail
+urllib.request.urlretrieve(url=blacklist, filename='blacklist_1kk.txt')
 
-nglsh = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
-urllib.request.urlretrieve(url=nglsh, filename='english.txt')
+english_list = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
+urllib.request.urlretrieve(url=english_list, filename='english.txt')
